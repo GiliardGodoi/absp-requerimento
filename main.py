5814443061b87b42bf827acf40a324439560b0da
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2020-06-01 08:52:20 -0300

    Configuration file and other stuff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import argparse
+import configparser
 import json
 import os
 from datetime import datetime
@@ -13,35 +14,35 @@ from src.worddocument import (relacionar_requerimentos,
 
 TODAY = datetime.now()
 
-# https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
 parser = argparse.ArgumentParser(description="Redator Oficial CLI")
-parser.add_argument("-A", "--ano", help="Ano entrada", type=str, default=str(TODAY.year))
-parser.add_argument("-M", "--mes", help="Mes entrada", type=str, default=number2Month[TODAY.month].upper())
-parser.add_argument("-d", "--dia", help="Dia entrada", type=str, required=True)
 
-parser.add_argument("--numerar",    help="Tenta numerar documentos", action='store_true')
-parser.add_argument("--excel",      help="Lê arquivo excel",         action='store_true')
-parser.add_argument("--json",       help="Lê arquivo json",          action='store_true')
-parser.add_argument("--relacionar", help="Relaciona os documentos em um arquivo Word", action='store_true')
-parser.add_argument("--relacionar2", help="Another option", action='store_true')
-# parser.add_argument("-P", "--planilha", help="Criar planilha de requerimentos", type=str2bool, default=False)
-# parser.add_argument("-I", "--imprimir", help="Imprimir na tela", type=str2bool, default=False)
-
+parser.add_argument("-A", "--ano", type=str, default=str(TODAY.year),
+                    help="Entrada Ano: informação para localizar diretório dos documentos")
+parser.add_argument("-M", "--mes", type=str, default=number2Month[TODAY.month].upper(),
+                    help="Entrada mes: informação para localizar diretório dos documetos")
+parser.add_argument("-d", "--dia", type=str, required=True,
+                    help="Entrada DIA: informação para localizar diretório dos documentos")
+parser.add_argument("--numerar", action='store_true',
+                    help="Tenta numerar documentos")
+parser.add_argument("--excel",   action='store_true',
+                    help="Lê arquivo excel")
+parser.add_argument("--json",    action='store_true',
+                    help="Lê arquivo json")
+parser.add_argument("--relacionar", action='store_true',
+                    help="Relaciona os documentos em um arquivo Word")
+parser.add_argument("--relacionar2", action='store_true',
+                    help="Outra opção para numerar requerimentos")
 
 args = parser.parse_args()
 
-print(args)
+
+## ===========================================
+config = configparser.ConfigParser()
+config.read("config.ini")
+
 ## ===========================================
 
-MAIN_FOLDER = os.path.join(r"C:\Users\User\Arquivo\R\REQUERIMENTOS", args.ano, args.mes, args.dia)
+MAIN_FOLDER = os.path.join(config["DEFAULT"]["start_folder"], args.ano, args.mes, args.dia)
 
 if not os.path.exists(MAIN_FOLDER):
     raise NotADirectoryError(f"Folder doesn't exist :: {MAIN_FOLDER}")
--- src/util.py
+++ src/util.py
@@ -49,5 +49,14 @@ requerimentos_modelo = set([
     "Todos.docx"
 ])
 
+# https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 def eh_requerimento_vereador(arquivo):
     return arquivo.endswith(".docx") and (arquivo in requerimentos_modelo)
